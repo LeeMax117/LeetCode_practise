7beeeb78eab468bf22cfbd425b623029cc554d89
nijilong1224 <42104427+nijilong1224@users.noreply.github.com>
2019-05-16 09:49:26 +0800

    Update monotone.py

--- Jinglong/monotone.py
+++ Jinglong/monotone.py
@@ -5,6 +5,7 @@ class Solution:
         for i in range(len(S)):
             if not start:
                 if S[i] == '1':
+                    # find the index of the first 1 encountered.
                     start, total, inmax, ind1 = True, -1, -1, i
             else:
                 if S[i] == '1':
@@ -12,7 +13,9 @@ class Solution:
                 else:
                     total += 1
                 if total > inmax:
+                    # record the most 0 in left sub-character array right end index.
                     inmax, ind2 = total, i
+        # calculate the count digits need to be reversed.
         if inmax > 0:
             dist = ind2 - ind1 + 1
             m = (dist - inmax) // 2
