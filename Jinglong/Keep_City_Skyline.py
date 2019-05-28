fdf227f6127d0c4318cad9338ef306209fd6b74c
nijilong1224 <42104427+nijilong1224@users.noreply.github.com>
2019-05-28 11:08:51 +0800

    Update Keep_City_Skyline.py

--- Jinglong/Keep_City_Skyline.py
+++ Jinglong/Keep_City_Skyline.py
@@ -2,11 +2,10 @@ class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
         tb, lr = [None] * len(grid), [None] * len(grid)
         old_skyline = 0
-        for r, row in enumerate(grid):
-            lr[r] = max(row)
+        for ind, row in enumerate(grid):
+            lr[ind] = max(row)
+            tb[ind] = max([v[ind] for v in grid])
             old_skyline += sum(row)
-        for c in range(len(grid)):
-            tb[c] = max([v[c] for v in grid])
         descartes = [min(vr, vc) for vr in lr for vc in tb]
         new_skyline = sum(descartes)
         return new_skyline - old_skyline
